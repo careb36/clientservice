708e5efee5edc45fdd6773043c4bb7a89ea9a0a4
careb36 <carolina.pereira@estudiantes.utec.edu.uy>
2023-11-23 21:04:15 -0300

    ok post and get JSON format

--- src/main/java/com/coderhouse/clientservice/controller/ClientController.java
+++ src/main/java/com/coderhouse/clientservice/controller/ClientController.java
@@ -3,7 +3,9 @@ package com.coderhouse.clientservice.controller;
 import com.coderhouse.clientservice.model.Client;
 import com.coderhouse.clientservice.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,12 +15,18 @@ public class ClientController {
 
     @Autowired
     private ClientService clientService;
+
     @PostMapping("/")
-        public ResponseEntity<Client> create (@RequestBody Client client){
-            return new ResponseEntity<>(this.clientService.create(client), HttpStatus.CREATED);
-        }
+    public ResponseEntity<Client> create(@RequestBody Client client) {
+        return new ResponseEntity<>(this.clientService.create(client), HttpStatus.CREATED);
+    }
+
     @GetMapping("/{id}")
-    public ResponseEntity<?> findById(@PathVariable Long id){
-        return new ResponseEntity<>(this.clientService.findById(id), HttpStatus.OK);
+    public ResponseEntity<String> findById(@PathVariable Long id) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        String result = clientService.findById(id);
+        return new ResponseEntity<>(result, headers, HttpStatus.OK);
     }
 }
--- src/main/java/com/coderhouse/clientservice/service/ClientService.java
+++ src/main/java/com/coderhouse/clientservice/service/ClientService.java
@@ -2,6 +2,8 @@ package com.coderhouse.clientservice.service;
 
 import com.coderhouse.clientservice.model.Client;
 import com.coderhouse.clientservice.repository.ClientRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,11 +19,21 @@ public class ClientService {
     public Client create(Client c) {
         return this.clientRepository.save(c);
     }
+    @Autowired
+    private ObjectMapper objectMapper; // Inyecta ObjectMapper
 
     public String findById(Long id) {
         return clientRepository.findById(id)
-                .map(c -> c.toString() + " Edad: " + calculateAge(c.getBirthDate()))
-                .orElse("Cliente no encontrado con ID: " + id);
+                .map(client -> {
+                    ObjectNode clientJson = objectMapper.valueToTree(client);
+                    clientJson.put("edad", calculateAge(client.getBirthDate()));
+                    try {
+                        return objectMapper.writeValueAsString(clientJson);
+                    } catch (Exception e) {
+                        throw new RuntimeException("Error al serializar a JSON", e);
+                    }
+                })
+                .orElse("{\"error\":\"Cliente no encontrado con ID: " + id + "\"}");
     }
 
     public int calculateAge(LocalDate birthDate) {
