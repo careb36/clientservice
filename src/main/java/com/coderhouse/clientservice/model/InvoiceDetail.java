7ade37c01ef5f271816bb57976dbee52a215ec26
Carolina <carolina.pereira@estudiantes.utec.edu.uy>
2023-11-27 00:28:09 -0300

    actualizo entidades

--- src/main/java/com/coderhouse/clientservice/model/InvoiceDetail.java
+++ src/main/java/com/coderhouse/clientservice/model/InvoiceDetail.java
@@ -1,4 +1,33 @@
 package com.coderhouse.clientservice.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+@Entity
+@Table(name = "invoices_details")
 public class InvoiceDetail {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "invoice_id")
+    private int invoiceId;
+
+    @Column(name = "product_id")
+    private int productId;
+
+    @Column(name = "quantity")
+    private int quantity;
+
+    @Column(name = "price")
+    private BigDecimal price;
+
+    public InvoiceDetail() {
+    }
 }
+
